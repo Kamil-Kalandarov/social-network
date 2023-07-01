adb3a3189286f8d3b854267c6e57f137454c7363
Kamil-Kalandarov <iam@kalandarov-13.ru>
2023-07-01 15:24:21 +0300

    comments added

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { setUserProfile } from '../../redux/profileReducer';
-import { openModal, closeModal } from '../../redux/modalReducer';
 import Profile from './Profile';
 
-
+/* контейнерная компонента, которая получает пропсы от другой контейнерной компоненты через connect 
+и передает их дальше в презентационную копоненту Profile */
 class ProfileContainer extends React.Component {
 
+  /* сначала отрабатывает render(), только потом монтирвоание компоненты и обновление стейта */
   componentDidMount() {
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
       .then(response => {
